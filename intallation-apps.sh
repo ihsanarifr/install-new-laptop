4d437240ca21e0ced52b922a1672be84410afe52
Ihsan Arif <ihsan.android@gmail.com>
2020-10-27 13:52:01 +0700

    ad docker and gvm

--- intallation-apps.sh
+++ intallation-apps.sh
@@ -1,7 +1,9 @@
 !bash
 
+$HOME = /home/ihsan/
+
 apt-get update -y
-apt install software-properties-common apt-transport-https wget git -y
+apt install software-properties-common apt-transport-https wget git curl -y
 
 ## install chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -14,3 +16,34 @@ sudo apt install code
 
 ## install zsh
 apt install zsh
+apt install
+
+## https://www.codepolitan.com/memasang-zsh-dan-oh-my-zsh-di-linux-ubuntu-5a8eb22e1ae13
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+source ~/.zshrc
+
+## https://github.com/moovweb/gvm
+sudo apt-get install binutils bison gcc make -y
+zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+source $HOME.gvm/scripts/gvm
+
+## set golang version
+gvm install go1.4 -B # https://github.com/moovweb/gvm/issues/286
+gvm use go1.4 [--default]
+
+## install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker ${USER}
+su -s ${USER}
+
+
+## install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
+printf '\nDocker Compose installed successfully\n\n'
